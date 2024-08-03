d50533a108124aeb72388c5ac0e63c4596c3daef
CrisOos <cristian.oostdijk@avenida.com>
2024-08-02 23:28:19 -0300

    react homepage

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => (
+  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+    <div className="jumbotron jumbotron-fluid bg-transparent">
+      <div className="container secondary-color">
+        <h1 className="display-4 text-center">Clothes Ecommerce</h1>
+        <p className="lead text-center">
+          A curated list of clothes for the best outfit.
+        </p>
+        <hr className="my-4" />
+        <div className="container secondary-color d-flex justify-content-between">
+          <Link
+            to="/clothes"
+            className="btn btn-lg custom-button mx-2"
+            role="button"
+          >
+            View Clothes
+          </Link>
+          <Link
+            to="/dashboard"
+            className="btn btn-lg custom-button mx-2"
+            role="button"
+          >
+            Go Dashboard
+          </Link>
+        </div>
+      </div>
+    </div>
+  </div>
+);
